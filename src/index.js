ff79e31e842e6dff14b188108b6274d43785e15a
Callum Mellor-Reed <me@callummr.com>
2017-08-09 22:03:55 +0100

    Let's try CRA's service worker
    the sw-precache one was refusing to update cache

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './IsItWednesdayMyDudes';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./IsItWednesdayMyDudes";
+import "./index.css";
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById("root"));
+
+registerServiceWorker();
