697333b1f17fcd103bc1ab1d5162725ade3e2215
Callum Mellor-Reed <me@callummr.com>
2017-09-27 14:11:30 +0100

    More than double the wednesday factor

--- src/videos/wednesday.js
+++ src/videos/wednesday.js
@@ -4,5 +4,13 @@ export default [
   "8G_w-YkujXs",
   "TMYlifKHakc",
   "DREDJ4fkz-g",
-  "du-TY1GUFGk"
+  "du-TY1GUFGk",
+  "Oct2xKMGOno",
+  "gxm5SwfkwcI",
+  "9K4-jllrPrE",
+  "j5crFeXzzp0",
+  "nUKQThJiDL0",
+  "R5ehbLdki0w",
+  "OWLJhxG9Ik0",
+  "T04h9xFQkBU"
 ];
