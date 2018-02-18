f113d1ec20c260b1672b565e3c7492993291dae3
Callum Mellor-Reed <me@callummr.com>
2018-02-18 08:01:44 +0000

    Never pick the same video twice

--- src/util/pickVideo.js
+++ src/util/pickVideo.js
@@ -1,22 +1,31 @@
 import videos from '../videos/wednesday';
 import pickRandom from './pickRandom';
 
-export default () => {
-  const queries = window.location.search
-    .replace('?', '')
-    .split('&')
-    .map(pair => pair.split('='))
-    .reduce((qs, next) => ({ ...qs, [next[0]]: next[1] }), {});
+let firstVideo = true;
+let lastId = '';
 
-  if (queries.v && videos.includes(queries.v)) return queries.v;
+export default () => {
+  if (firstVideo) {
+    firstVideo = false;
+
+    const queries = window.location.search
+      .replace('?', '')
+      .split('&')
+      .map(pair => pair.split('='))
+      .reduce((qs, next) => ({ ...qs, [next[0]]: next[1] }), {});
+
+    if (queries.v && videos.includes(queries.v)) {
+      return (lastId = queries.v);
+    }
+  }
 
   const newestWatched = localStorage.getItem('newestWatched');
   const newestVideo = videos[videos.length - 1];
 
   if (!newestWatched || newestWatched !== newestVideo) {
     localStorage.setItem('newestWatched', newestVideo);
-    return newestVideo;
+    return (lastId = newestVideo);
   }
 
-  return pickRandom(videos);
+  return (lastId = pickRandom(videos.filter(videoId => videoId !== lastId)));
 };
