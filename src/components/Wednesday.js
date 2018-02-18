803d7b87a5f2512f6b906872fd11a16b1ecb2d3a
Callum Mellor-Reed <me@callummr.com>
2018-02-18 08:02:12 +0000

    Play videos forever

--- src/components/Wednesday.js
+++ src/components/Wednesday.js
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
+import YouTube from 'react-youtube';
 import wednesdayImage from '../assets/wednesday.jpg';
 import wednesdayText from '../util/wednesdayText';
 import pickVideo from '../util/pickVideo';
 import VideoEmbed from './VideoEmbed';
 
-export default () =>
-  navigator.onLine ? (
-    <VideoEmbed videoId={pickVideo()} />
-  ) : (
-    <div className="align-center">
-      <img src={wednesdayImage} alt={wednesdayText()} />
-    </div>
-  );
+export default class wednesday extends Component {
+  state = {
+    videoId: pickVideo(),
+  };
+
+  playNextVideo = () =>
+    this.setState(({ videoId }) => ({
+      videoId: pickVideo(),
+    }));
+
+  render() {
+    return navigator.onLine ? (
+      <div className="video-background">
+        <div className="video-foreground">
+          <YouTube
+            videoId={this.state.videoId}
+            opts={{ playerVars: { autoplay: 1, controls: 0 } }}
+            onEnd={this.playNextVideo}
+          />
+        </div>
+      </div>
+    ) : (
+      <div className="align-center">
+        <img src={wednesdayImage} alt={wednesdayText()} />
+      </div>
+    );
+  }
+}
