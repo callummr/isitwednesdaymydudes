e92eacfeb12e2b14b1292314c7e891177aa0c1bf
Callum Mellor-Reed <me@callummr.com>
2018-08-07 20:23:46 +0100

    Correct typos

--- update-videos.js
+++ update-videos.js
@@ -5,13 +5,13 @@ import currentVideoIds from './src/videos/wednesday'
 dotenv.config()
 
 const { YOUTUBE_KEY } = process.env
-if (!YOUTUBE_KEY) throw new Error("YOUTUBE_KEY must an environment variable")
+if (!YOUTUBE_KEY) throw new Error("YOUTUBE_KEY must be an environment variable")
 
 const apiUrl = 'https://www.googleapis.com/youtube/v3/playlistItems'
 
 const queryItems = {
   key: YOUTUBE_KEY,
-  playlistId: 'PLy3-VH7qrUZ5IVq_lISnoccVIYZCMvi-8', // zimonitrome wednesday vids
+  playlistId: 'PLy3-VH7qrUZ5IVq_lISnoccVIYZCMvi-8', // zimonitrome Wednesday vids
   part: 'contentDetails', // videoId is in here
   maxResults: 50, // max allowed, ok if we run regularly
   order: 'date', // we want the most recent uploads
@@ -31,7 +31,7 @@ request({
   const newVideoIds = playlistVideoIds.filter(id => !currentVideoIds.includes(id))
   
   if (newVideoIds.length) {
-    // looks like it is wednesday, my dudes
+    // looks like it is Wednesday, my dudes
     writeNewVideoFile([...currentVideoIds, ...newVideoIds])
   }
 })
